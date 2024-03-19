85dca04d4811771b8b16757557d8abc85eecf6bf
Elvis Kemoi <elviskemoi001@gmail.com>
2024-03-19 10:57:31 +0300

    Hero section complete. Added animation text.

---
+++ index.js
@@ -0,0 +1,26 @@
+const words = [
+	"Software Engineer<i class='bx bxs-star-half'></i>",
+	"Web Developer<i class='bx bx-star' ></i>",
+	"Graphic Designer<i class='bx bxs-star' ></i>",
+];
+
+//Main timeline
+let mainTimeline = gsap.timeline({
+	repeat: -1,
+});
+
+//For each word, create a new timeline, use the Text plugin, then append that timeline to the main one
+words.forEach((word) => {
+	let textTimeline = gsap.timeline({
+		repeat: 1,
+		yoyo: true,
+		repeatDelay: 2,
+	});
+
+	textTimeline.to("#typewriter", {
+		text: word,
+		duration: 1,
+	});
+
+	mainTimeline.add(textTimeline);
+});
